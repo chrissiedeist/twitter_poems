898a91df8ee8f01548320a22c4c3c401195cb47f
Chrissie Deist <chrissie.deist@gmail.com>
2016-06-11 16:51:41 -0500

    Refactor twitter service
    This refactor allows for easy addition of new regexes for
    things to be removed from the returned tweet text.

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,4 +1,12 @@
 class TwitterService
+  REGEXES_TO_REMOVE_FROM_TEXT = {
+    :urls => /http\S*/,
+    :retweet => /^RT .*: /,
+    :handles => /@\S*/,
+  }
+
+  NUM_TWEETS_TO_FETCH = 20
+
   def initialize
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['CONSUMER_KEY']
@@ -9,25 +17,31 @@ class TwitterService
   end
 
   def text_from_query(query)
-    query.sub(/#/, '')
-    results = @client.search("##{query}", count: 20) 
+    results = _get_raw_results(query)
 
-    text = results.map(&:text).map do |text| 
-      text = _remove_url(text)
-      text = _remove_retweet(text)
-      _remove_handles(text)
+    return nil unless results
+
+    _get_cleaned_text(results)
+  end
+
+  def _get_raw_results(query) 
+    query = _add_hashtag_if_missing(query)
+    @client.search(query, count: NUM_TWEETS_TO_FETCH) 
+  end
+
+  def _add_hashtag_if_missing(query)
+    query.match(/^#/) ? query : "##{query}"
+  end
+
+  def _get_cleaned_text(results)
+    results.map(&:text).map do |text| 
+      _remove_unwanted_text(text)
     end.join("")
   end
 
-  def _remove_url(tweet)
-    tweet.gsub(/http\S*/, '')
-  end
+  def _remove_unwanted_text(tweet)
+    regex = Regexp.union(REGEXES_TO_REMOVE_FROM_TEXT.values)
 
-  def _remove_retweet(tweet)
-    tweet.sub(/^RT .*: /, '')
+    tweet.gsub(regex, '')
   end
-
-   def _remove_handles(tweet)
-     tweet.gsub(/@\S*/, '')
-   end
 end
--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -2,42 +2,67 @@ require('rails_helper')
 
 describe TwitterService do
   describe "self.text_from_query" do
+    before(:each) do
+      @twitter_service = TwitterService.new
+    end
+
     it "returns the text from tweets returned from a search" do
-      result = TwitterService.new.text_from_query("ruby")
+      result = @twitter_service.text_from_query("ruby")
       expect(result.downcase).to match(/ruby/)
     end
 
-    it "removes urls from text" do
-      tweet = double(:text => "Saturday’s Pet of the Day is #fun Tiki-a #pineapple #conure -talk of him\nhttps://t.co/3S54PjvAne\n#petoftheday #pets https://t.co/j26c0cUwqw")
+    it "returns nil if there are no results from twitter" do
+        expect_any_instance_of(Twitter::REST::Client).to receive(:search).and_return nil
 
-      allow_any_instance_of(Twitter::REST::Client).to receive(:search).and_return([tweet])
-
-      result = TwitterService.new.text_from_query("fun")
-
-      expect(result.downcase).to match(/fun/)
-      expect(result.downcase).to_not match(/http/)
+        result = @twitter_service.text_from_query("xasdfasdgagds")
+        expect(result).to be_nil
     end
 
-    it "removes retweet portion of tweet" do
-      tweet = double(:text => "RT @twitterhandle: Saturday’s Pet of the Day is so fun https://t.co/3S54PjvAne\n#petoftheday #pets https://t.co/j26c0cUwqw")
+    it "adds a # in front of the query if it doesn't contain one" do
+        expect_any_instance_of(Twitter::REST::Client).to receive(:search).with("#fun", {:count => 20 })
 
-      allow_any_instance_of(Twitter::REST::Client).to receive(:search).and_return([tweet])
-
-      result = TwitterService.new.text_from_query("fun")
-
-      expect(result.downcase).to match(/fun/)
-      expect(result.downcase).to_not match(/^RT/)
+        @twitter_service.text_from_query("fun")
     end
 
-    it "removes twitter handles" do
-      tweet = double(:text => "@twitterhandle: Saturday’s Pet of the Day is so fun @anotherhandle")
+    it "does not add an extra # in front of the query if already contains one" do
+        expect_any_instance_of(Twitter::REST::Client).to receive(:search).with("#fun", {:count => 20})
 
-      allow_any_instance_of(Twitter::REST::Client).to receive(:search).and_return([tweet])
+        @twitter_service.text_from_query("#fun")
+    end
 
-      result = TwitterService.new.text_from_query("fun")
+    context "when tweets contain unwanted text" do
+      it "removes urls from text" do
+        tweet = double(:text => "Saturday’s Pet of the Day is #fun Tiki-a #pineapple #conure -talk of him\nhttps://t.co/3S54PjvAne\n#petoftheday #pets https://t.co/j26c0cUwqw")
 
-      expect(result.downcase).to match(/fun/)
-      expect(result.downcase).to_not match(/@/)
+        allow_any_instance_of(Twitter::REST::Client).to receive(:search).and_return([tweet])
+
+        result = @twitter_service.text_from_query("fun")
+
+        expect(result.downcase).to match(/fun/)
+        expect(result.downcase).to_not match(/http/)
+      end
+
+      it "removes retweet portion of tweet" do
+        tweet = double(:text => "RT @twitterhandle: Saturday’s Pet of the Day is so fun https://t.co/3S54PjvAne\n#petoftheday #pets https://t.co/j26c0cUwqw")
+
+        allow_any_instance_of(Twitter::REST::Client).to receive(:search).and_return([tweet])
+
+        result = @twitter_service.text_from_query("fun")
+
+        expect(result.downcase).to match(/fun/)
+        expect(result.downcase).to_not match(/^RT/)
+      end
+
+      it "removes twitter handles" do
+        tweet = double(:text => "@twitterhandle: Saturday’s Pet of the Day is so fun @anotherhandle")
+
+        allow_any_instance_of(Twitter::REST::Client).to receive(:search).and_return([tweet])
+
+        result = @twitter_service.text_from_query("fun")
+
+        expect(result.downcase).to match(/fun/)
+        expect(result.downcase).to_not match(/@/)
+      end
     end
   end
 end
