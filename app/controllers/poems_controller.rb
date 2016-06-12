ce237d593d68bedb3ba999fe304c7d02da685509
Chrissie Deist <chrissie.deist@gmail.com>
2016-06-12 08:13:38 -0500

    Handle errors from Twitter in controller

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,4 +1,9 @@
 class PoemsController < ApplicationController
+  rescue_from Twitter::Error::Forbidden, with: :bad_request
+  rescue_from Twitter::Error::BadRequest, with: :bad_request
+  rescue_from Twitter::Error::TooManyRequests, with: :too_many_requests
+  rescue_from Twitter::Error::Unauthorized, with: :unauthorized
+
   def create
     results = TwitterService.text_from_query(params[:query])
 
@@ -9,5 +14,22 @@ class PoemsController < ApplicationController
 
   def new
     @trending_topics = TwitterService.trending_topics || []
+  end
+
+  private
+
+  def bad_request
+    flash[:error] = TwitterService::BAD_REQUEST_MESSAGE
+    render 'error'
+  end
+
+  def too_many_requests
+    flash[:error] = TwitterService::RATE_LIMIT_ERROR_MESSAGE
+    render 'error'
+  end
+
+  def unauthorized 
+    flash[:error] = TwitterService::UNAUTHORIZED_MESSAGE
+    redirect_to :new_poem
   end
 end
--- spec/controllers/poems_controller_spec.rb
+++ spec/controllers/poems_controller_spec.rb
@@ -4,11 +4,30 @@ RSpec.describe PoemsController, type: :controller do
   describe "create" do
     it "creates a new Poem" do
       allow(TwitterService).to receive(:text_from_query).and_return("tweeted stuff")
-      post :create, :query => "#ruby" 
+
+      post :create, :query => "ruby"
 
       expect(response).to have_http_status(:created)
       poem = assigns(:poem)
       expect(poem).to be_a(Poem)
+    end
+
+    it "handles authentication error from Twitter" do
+      TwitterService.stub(:text_from_query).and_raise(Twitter::Error::BadRequest)
+
+      post :create, :query => "ruby" 
+
+      expect(response).to render_template(:error)
+      expect(flash[:error]).to include(TwitterService::BAD_REQUEST_MESSAGE)
+    end
+
+    it "handles rate limiting errors from Twitter" do
+      TwitterService.stub(:text_from_query).and_raise(Twitter::Error::TooManyRequests)
+
+      post :create, :query => "ruby" 
+
+      expect(response).to render_template(:error)
+      expect(flash[:error]).to include(TwitterService::RATE_LIMIT_ERROR_MESSAGE)
     end
   end
 end
