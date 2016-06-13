5e89ea27e760b7b64fef753586b9737f424ae573
Chrissie Deist <chrissie.deist@gmail.com>
2016-06-12 23:24:09 -0500

    Remove access tokens
    Use application-only authentication to
    allow for higher rate limiting

--- app/modules/twitter_helper.rb
+++ app/modules/twitter_helper.rb
@@ -2,8 +2,6 @@ module TwitterHelper
   TWITTER_CLIENT_CONFIG = {
      :consumer_key => ENV['CONSUMER_KEY'],
      :consumer_secret => ENV['CONSUMER_SECRET'],
-     :access_token => ENV['ACCESS_TOKEN'],
-     :access_token_secret => ENV['ACCESS_TOKEN_SECRET'],
   }
 
   def self.authenticated_twitter_client
